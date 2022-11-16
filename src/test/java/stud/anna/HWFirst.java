69744e89ebde58e3f37db054cb29860fdecb81c3
Anna Petrova <mail.anna.p@yandex.ru>
2022-11-16 23:56:18 +0500

    add

---
+++ src/test/java/stud/anna/HWFirst.java
@@ -0,0 +1,13 @@
+package stud.anna;
+
+import java.io.FilterOutputStream;
+
+public class HWFirst {
+    public static void main(String[] args) {
+        byte varF = 100;
+        varF = 100 + 40;
+        System.out.println("varF");
+
+    }
+
+}
